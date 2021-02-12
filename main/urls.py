11d91b400f787f64b0f4aeaef58eff33c3f62ea8
Vincent Chan <vinccodes@gmail.com>
2021-02-11 22:21:26 -0500

    Finished article details functionality

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 from main import views 
 urlpatterns = [
     path('', views.articles_list, name="articles-list"),
+    path('articles/<int:article_id>/', views.article_details, name="articles-detail"),
 ]
--- main/views.py
+++ main/views.py
@@ -9,3 +9,10 @@ def articles_list(request):
         "articles": articles,
     }
     return render(request, "articles_list.html", context)
+
+def article_details(request, article_id):
+    context = {
+        "article": Article.objects.get(id=article_id)
+    }
+    
+    return render(request, 'article_details.html', context)
